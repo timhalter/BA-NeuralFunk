439acb35b8d0aeac6130c0958fafe0a3f32833c7
timhalter <80471545+timhalter@users.noreply.github.com>
2022-12-19 15:29:44 +0100

    Add files via upload

---
+++ DeepDrummer/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='deepdrummer',
+    version='1.0',
+    packages=[
+        'deepdrummer',
+        'deepdrummer.webserver',
+    ],
+    install_requires=[
+        'numpy',
+        'torch',
+        'torchaudio',
+        'six',
+        'soundfile',
+        'sounddevice',
+        'scipy',
+        'matplotlib',
+        'pyyaml'
+    ]
+)
