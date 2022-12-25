4d91aa04c768c4881dffe1ac9437693646e2e5df
Peter Blattmann <61541096+Cr4ckii@users.noreply.github.com>
2022-12-25 16:03:38 +0100

    Update pattern_serializer.py

--- DeepDrummer/deepdrummer/pattern_serializer.py
+++ DeepDrummer/deepdrummer/pattern_serializer.py
@@ -13,10 +13,6 @@ def get_audio_param_bp():
 
     return audio_params, audio_pattern_bp   
 
-def get_input_sample_rate():
-    audio_params, _  = get_audio_param_bp()
-    return audio_params['sample_rate_input']
-
 def get_num_of_lines():
     audio_params, _  = get_audio_param_bp()
     return audio_params['nbr_of_lines']
